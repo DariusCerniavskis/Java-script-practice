8a8d2772d8ba6bf3fc3b398e7de633f6399c93e5
DariusCerniavskis <Darius.Cerniavskis@gmail.com>
2025-12-18 11:19:37 +0200

    Task4

---
+++ Sprendimai/Sprendimas4/java-script.js
@@ -0,0 +1,12 @@
+const drawComb = (lenght) => {
+    for (i = 0; i < lenght; i++) {
+        console.log("#".repeat(i % 2 === 0 ? 10 : 1));
+    }
+    console.log("#".repeat(lenght % 2 === 0 ? 10 : 0));
+};
+
+combLenght = Number(prompt("What is comb lenght ?"));
+
+drawComb(combLenght);
+
+console.log(`The task is done`);
