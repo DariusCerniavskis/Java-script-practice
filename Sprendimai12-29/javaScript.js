ce1b54bc9217557ff31b62b8e295c3da87e69dc5
DariusCerniavskis <Darius.Cerniavskis@gmail.com>
2025-12-29 09:51:48 +0200

    tasks 1-3

---
+++ Sprendimai12-29/javaScript.js
@@ -0,0 +1,11 @@
+// 1. Atspauzdinti ekrane teksto "Sveiki!, cia 12 paskaita." ilgį;
+const fraze1 = "Sveiki!, cia 12 paskaita.";
+console.log("1. " + fraze1.length);
+
+// 2. Atspauzdinti kurioje pozicijoje yra ženklas "!";
+const fraze2 = "Sveiki!, cia 12 paskaita.";
+console.log("2. " + fraze2.indexOf("!"));
+
+// 3. Tekstą "12" pakeist į "22";
+const fraze3 = "12";
+console.log("3. " + fraze3.replace("1", "2"));
