6fa588c2dfaa49465e7284b81d008b946a19bb0a
DariusCerniavskis <Darius.Cerniavskis@gmail.com>
2025-12-29 09:59:36 +0200

    tasks 1-5

--- Sprendimai12-29/javaScript.js
+++ Sprendimai12-29/javaScript.js
@@ -9,3 +9,14 @@ console.log("2. " + fraze2.indexOf("!"));
 // 3. Tekstą "12" pakeist į "22";
 const fraze3 = "12";
 console.log("3. " + fraze3.replace("1", "2"));
+
+// 4. "     Atvaizduot šį tekstą ištrinus tarpus    " ;
+const fraze4 = "     Atvaizduot šį tekstą ištrinus tarpus    ";
+console.log("4. " + fraze4.trim());
+
+// 5. Sukurt sakinį. Parašyt kodą kuris kiekvieną sakinio žodį įdeda į atskirą masyvo elementą;
+const fraze5 =
+    "Sukurt sakinį. Parašyt kodą kuris kiekvieną sakinio žodį įdeda į atskirą masyvo elementą";
+console.log("5.");
+const wordArray = fraze5.split(" ");
+console.log(wordArray);
