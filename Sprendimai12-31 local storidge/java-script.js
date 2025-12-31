7b3520ec230ea0766621cbd767fe63f6e486a3d7
DariussCerniavskis <DariusCerniavskis@gmail.com>
2025-12-31 11:40:51 +0200

    Update 12-31

---
+++ Sprendimai12-31 local storidge/java-script.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+const inputObj=document.getElementById("userInput");
+const acceptObj=document.getElementById("acceptButton")
+
+
+acceptObj.addEventListener("click",()=>{
+     console.log("user name: "+inputObj.value)
+})
