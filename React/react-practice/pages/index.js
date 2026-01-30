bf31e213120f0f9011a87ffb566cc19092dc6bb7
ErikasCerniavskis <altforuse111@gmail.com>
2026-01-30 11:54:12 +0200

    update 01-30

---
+++ React/react-practice/pages/index.js
@@ -0,0 +1,18 @@
+
+import styles from "../styles/main.module.css";
+
+const Main = () => {
+  return (
+    <>
+      <header className={styles.header}>1</header>
+      <main className={styles.main}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </main>
+      <footer className={styles.footer}>3</footer>
+    </>
+  );
+};
+
+export default Main;
