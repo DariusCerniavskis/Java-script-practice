77886e901107219775dadce5d1f816f7c512dc3a
DariusCerniavskis <Darius.Cerniavskis@gmail.com>
2026-02-03 17:24:08 +0200

    Variables in components 02-03

---
+++ React_02-03_kintamieji/react_practice_02-03_kintamieji/components/Header/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+const Header = ({ navName }) => {
+    return (
+        <>
+            <h4>{navName}</h4>
+        </>
+    );
+};
+
+export default Header;
